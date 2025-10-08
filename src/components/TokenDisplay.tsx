fe6dc8d793c9f1a1d855626380f8bf40907b6921
MarnelleApat <marnelle.apat@bible.org.sg>
2025-10-08 10:07:32 +0800

    adjust tokenDisplay styling

--- src/components/TokenDisplay.tsx
+++ src/components/TokenDisplay.tsx
@@ -19,11 +19,11 @@ export default function TokenDisplay() {
   }
 
   return (
-    <div className="flex items-center shadow drop-shadow space-x-2 bg-gradient-to-r from-orange-100 via-orange-50 to-amber-100 dark:from-orange-700/20 dark:via-orange-500/20 dark:to-amber-700/20 dark:shadow-md dark:shadow-orange-800/20 dark:border dark:border-orange-800/40 rounded-lg px-3 py-2">
+    <div className="flex items-center shadow drop-shadow space-x-2 bg-gradient-to-r from-orange-100 via-orange-50 to-amber-100 dark:from-orange-800/50 dark:via-orange-700/50 dark:to-amber-400/50 dark:shadow-md dark:shadow-orange-800/20 rounded-lg px-3 py-2">
       <Coins className="h-4 w-4 text-orange-400 drop-shadow dark:text-orange-400" />
       <div className="text-xs">
-        <div className="font-medium text-orange-800 dark:text-orange-200">
-           {formatTokenNumber(tokensUsed)}/{formatTokenNumber(tokenBalance)} tokens
+        <div className="font-light tracking-widest text-[11px] text-orange-800 dark:text-orange-200">
+           {formatTokenNumber(tokensUsed)}/{formatTokenNumber(tokenBalance)} TOKENS
         </div>
       </div>
     </div>
